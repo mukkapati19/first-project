39e3aeb446c2ba63680055b3b3763887fe2d657d
mukkapati lala <mukkapati lala>
2017-02-03 18:48:17 -0600

    Initial commit

---
+++ src/com/durgasoft/service/CalService.java
@@ -0,0 +1,17 @@
+package com.durgasoft.service;
+
+public class CalService {
+	
+	public int add(int a,int b){
+		
+		return a+b;
+		
+	}
+	
+public int sub(int a,int b){
+		
+		return a-b;
+		
+	}
+
+}
